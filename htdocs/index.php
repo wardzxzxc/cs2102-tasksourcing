d83dbda471cbf80a4bf5b39eb5234071941e3241
wardzxzxc <wardzxzxc@gmail.com>
2018-09-15 13:37:59 +0800

    Initial template

---
+++ htdocs/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>  
+<head>
+  <title>Home</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <style>li {list-style: none;}</style>
+</head>
+<body>
+  <h2>Task Creation</h2>
+  <ul>
+    <li><a href="/search.php">Create Tasks></a></li>
+    <li><a href="">Read Tasks<</li>
+    <li><a href="/add.php">Update Tasks</a></li>
+    <li><a href="">Delete Task</a></li>
+  </ul>
+</body>
+</html>
