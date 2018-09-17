e0b5e192cc2712d0e177c4e2bbf3f33e9d789453
wardzxzxc <wardzxzxc@gmail.com>
2018-09-18 02:48:36 +0800

    changed links in index and upload create.php

---
+++ htdocs/create.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>  
+<head>
+  <title>Create User</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <style>li {list-style: none;}</style>
+</head>
+<body>
+  <h2>Create User</h2>
+  <ul>
+    <form name='display' action='create.php' method='POST' >  
+      <li>User ID:</li>  
+      <li><input type='text' name='user_id'/></li>  
+      <li>First Name:</li>  
+      <li><input type='text' name='first_name'/></li>  
+      <li>Last Name:</li>
+      <li><input type='text' name='last_name'/></li> 
+      <li>Email:</li>  
+      <li><input type='text' name='email'/></li>
+      <li>Phone:</li>  
+      <li><input type='text' name='phone'/></li>
+      <li>Password:</li>  
+      <li><input type='text' name='password'/></li>
+      <li>Zip Code:</li>  
+      <li><input type='text' name='zipcode'/></li>
+      <li>Bid Point:</li>  
+      <li><input type='text' name='bid_point'/></li>
+      <li>Admin:</li>  
+      <li><input type='text' name='is_admin'/></li>
+      <li><input type='submit' name='create' value='Create'/></li>  
+    </form>
+  </ul>
+  
+  <?php
+    session_start();
+    include('connection.php');
+
+    if (isset($_POST['create'])) {  // Submit the insert SQL command
+        $result = pg_query($db, "INSERT INTO users VALUES ('$_POST[user_id]', '$_POST[first_name]',
+        '$_POST[last_name]', '$_POST[email]', '$_POST[phone]', '$_POST[password]',
+        '$_POST[zipcode]', '$_POST[bid_point]', '$_POST[is_admin]')");
+        if (!$result) {
+            echo "User not created";
+        } else {
+            echo "User created successfully";
+        }
+    }
+    ?>  
+</body>
+</html>
