d82439dc7a66f96f0583defc8f3773682a3842d7
kebearry <kebearry@gmail.com>
2018-09-17 14:57:42 +0800

    add delete function

---
+++ htdocs/delete.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title>DELETE user with PHP</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <style>li {list-style: none;}</style>
+  </head>
+
+  <body>
+    <h2>Supply user Id you would like to delete and enter:</h2>
+    <ul>
+      <form name="display" action="delete.php" method="POST" >
+        <li>User ID:</li>
+        <li><input type="text" name="userid" /></li>
+        <li><input type="submit" name="submit" /></li>
+      </form>
+    </ul>
+    <?php
+      session_start();
+      include('connection.php');
+
+      if (isset($_POST['submit'])) {
+
+    		$id = $_POST[userid];
+    		$query = "DELETE FROM people WHERE userid = $id";
+        $execute = pg_query ($db, $query);
+
+    		if (!$execute) {
+          echo "Problem with removing user: $id, please try again.";
+    		} else {
+    			echo "Successfully removed user!";
+    		}
+      }
+    ?>
+  </body>
+
+</html>
