b45429b0687ac879b12f970061be849c86daea7c
jiahuipuah <43085761+jiahuipuah@users.noreply.github.com>
2018-09-19 00:25:50 +0800

    Update index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,11 +5,11 @@
   <style>li {list-style: none;}</style>
 </head>
 <body>
-  <h2>Task Creation</h2>
+  <h2>User CRUD</h2>
   <ul>
-      <button><a href="/create.php">Create Task</a></button><br/>
-  	  <button><a href="/readupdate.php">Read/Update Task</a></button><br/>
-  	  <button><a href="/delete.php">Delete Task</a></button>
+      <button><a href="/create.php">Create User</a></button><br/>
+  	  <button><a href="/readupdate.php">Read/Update Users</a></button><br/>
+  	  <button><a href="/delete.php">Delete User</a></button>
   </ul>
 </body>
 </html>
