1fbda28fe9cce84367208711e274c4888494a22c
jiahuipuah <43085761+jiahuipuah@users.noreply.github.com>
2018-09-26 23:09:32 +0800

    Update schema.sql
    Updated email in users table to be UNIQUE

--- database/schema.sql
+++ database/schema.sql
@@ -2,7 +2,7 @@ CREATE table users (
 	user_id SERIAL PRIMARY KEY NOT NULL,
 	first_name VARCHAR (64) NOT NULL,
 	last_name VARCHAR (64) NOT NULL,
-	email VARCHAR (256) NOT NULL,
+	email VARCHAR (256) UNIQUE NOT NULL,
 	phone CHARACTER (8) NOT NULL,
 	password VARCHAR (64) NOT NULL CONSTRAINT too_short CHECK(char_length(password) >= 8),
 	zipcode NUMERIC NOT NULL,
