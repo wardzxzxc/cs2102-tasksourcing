66f90fefa35e929d92a4c343b01cbb7c009fe44e
wardzxzxc <wardzxzxc@gmail.com>
2018-10-12 00:12:09 +0800

    Edited create task

--- htdocs/createTask.php
+++ htdocs/createTask.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html>
+<?php
+  session_start();
+  include('connection.php');
+  $curUser = $_SESSION["user"];
+?>
 <head>
 
   <title>Create Task</title>
@@ -36,9 +41,6 @@ li {
         <ul>
     <form name="display" action="createTask.php" method="POST" >
         <p><input class="w3-input w3-padding-small w3-border" type='text'
-            placeholder="Task ID" name='task_id'/></p>
-
-        <p><input class="w3-input w3-padding-small w3-border" type='text'
             placeholder="Task Cost" name='task_cost'/></p>
 
         <p><input class="w3-input w3-padding-small w3-border" type='text'
@@ -48,10 +50,7 @@ li {
             placeholder="Task Description" name='task_description'/></p>
 
         <p><input class="w3-input w3-padding-small w3-border" type='text'
-            placeholder="Created" name='task_datetime_created' value = '<?php echo date("Y-m-d H:i:s")?>'/></p>
-
-        <p><input class="w3-input w3-padding-small w3-border" type='text'
-            placeholder="Task Zipcode" name='task_zipcode'/></p>
+            placeholder="Address" name='task_zipcode'/></p>
 
         <p><input class="w3-input w3-padding-small w3-border" type='text'
             placeholder="Task Duration (in hour)" name='task_duration'/></p>
@@ -59,24 +58,33 @@ li {
         <p><input class="w3-input w3-padding-small w3-border" type='text'
             placeholder="Task Start (eg. 2016-12-25 00:00:00)" name='task_starttime'/></p>
 
-        <p><select class="w3-light-grey w3-dropdown-click w3-padding-16 w3-border" name = "is_available">
-          <option> Is Task Available? </option>
-          <option value = "Yes"> Yes </option>
-          <option value = "No"> No </option>
-        </select></p>
+        <p>
+            <select name="task_catalogue">
+                <?php
+                $result1 = pg_query($db, "SELECT name FROM catalogue");                                
+                while($row = pg_fetch_array($result1)) {
+                  echo   "<option value = " . $row["name"] . ">" . $row["name"] . " </option>";
+
+                }?>
+            </select>
+        </p>
 
         <p><input class="w3-button w3-black" type='submit' name='createTask'/></p>
     </form>
         </br>
-    <button class="w3-button w3-black"><a href="index.php">Main Menu</a></button>
+    <button class="w3-button w3-black"><a href="/dashboard.php">Main Menu</a></button>
         </ul>
 
   <?php
-    session_start();
-    include('connection.php');
 
     if (isset($_POST['createTask'])) {  // Submit the insert SQL command
-        $result = pg_query($db, "INSERT INTO task (task_code, task_title, task_description, task_datetime_created, tast_zipcode, task_duration, task_starttime, is_available) VALUES ('$_POST[task_cost]', '$_POST[task_title]', '$_POST[task_description]', '$_POST[task_datetime_created]', '$_POST[task_zipcode]','$_POST[task_duration]', '$_POST[task_starttime]', '$_POST [is_available]')");
+        $date = date('Y-m-d H:i:s');
+
+        $result = pg_query($db, "INSERT INTO task (task_cost, task_title, task_description, task_datetime_created, task_zipcode,
+                     task_duration, task_starttime, is_available, task_owner, task_catalogue) VALUES
+                     ('$_POST[task_cost]', '$_POST[task_title]', '$_POST[task_description]', '$date', '$_POST[task_zipcode]',
+                     '$_POST[task_duration]', '$_POST[task_starttime]', '1', '$curUser', '$_POST[task_catalogue]')");
+
         if (!$result) {
             echo '<div class="message"> Task not created </div>';
         } else {
